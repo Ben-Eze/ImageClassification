1416d5d8f8d319d8a3292a46558801f4dd2aaf3c
Ben-Eze <benedicteze@gmail.com>
2024-12-25 21:23:57 +0000

    More consistent save file name

--- src/save_load.py
+++ src/save_load.py
@@ -7,14 +7,12 @@ def save(model, file_path, loss=None, acc=None, training_complete=None):
     path = Path(file_path)
     dir = path.parent
 
-    # This reduces the chance of overwriting a model accidentally
-    # For more important training on bigger models, would want to ensure unique 
-    # file names to eliminate the risk of overwriting
-    if training_complete is not None and not training_complete:
-        fname = path.name
-        file_path = f"{dir}/INTERRUPTED-{fname}"
+    # if training_complete is not None and not training_complete:
+    #     fname = path.name
+    #     file_path = f"{dir}/INTERRUPTED-{fname}"
 
     os.makedirs(dir, exist_ok=True)
+
     if file_path.endswith(".pt"):
         file_path = file_path[:-3]
     
@@ -23,12 +21,16 @@ def save(model, file_path, loss=None, acc=None, training_complete=None):
 
     if acc is not None:
         file_path += f",acc={acc:.4f}"
-        file_path += ".pt"
+
+    if training_complete is not None:
+        file_path += f",train_complete={training_complete}"
+    
+    file_path += ".pt"
     
     torch.save(model.state_dict(), file_path)
     
     print(f"Model ({get_num_params(model)}-parameter {type(model)}) " 
-          f"saved successfully to {file_path}")
+          f"saved successfully to '{file_path}'")
 
 
 def get_num_params(model):
