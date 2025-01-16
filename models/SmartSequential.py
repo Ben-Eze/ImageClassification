163dfb28d8975774cc859d2235e1055c626dcbd1
Ben-Eze <benedicteze@gmail.com>
2025-01-16 00:15:40 +0000

    Improve control over Conv layers & add more activation functions to SmartSequantial

--- models/SmartSequential.py
+++ models/SmartSequential.py
@@ -1,11 +1,22 @@
 from torch import nn
 
 
+def parseConv2d(x: list):
+    # set Conv2d parameters to those specified in x otherwise use default values
+    return nn.Conv2d(in_channels=x[0],
+                     out_channels=x[1],
+                     kernel_size=3 if len(x) < 3 else x[2],
+                     stride=1 if len(x) < 4 else x[3],
+                     padding=1 if len(x) < 5 else x[4])
+
+
 class SmartSequential(nn.Module):
     module_dict = {}
 
     activation_functions = {
          "relu": nn.ReLU(),
+         "leakyrelu": nn.LeakyReLU(negative_slope=0.01),
+         "gelu": nn.GELU(),
          "tanh": nn.Tanh(),
     }
 
@@ -19,11 +30,7 @@ class SmartSequential(nn.Module):
     config2layer = {
         "L": lambda x: nn.Linear(in_features=x[0], 
                                  out_features=x[1]),
-        "C": lambda x: nn.Conv2d(in_channels=x[0],
-                                 out_channels=x[1],
-                                 kernel_size=3,
-                                 stride=1,
-                                 padding=1),
+        "C": lambda x: parseConv2d(x),
         "A": lambda x: SmartSequential.activation_functions[x],
         "F": lambda x: nn.Flatten(start_dim=x),
         "M": lambda x: nn.MaxPool2d(kernel_size=x),
